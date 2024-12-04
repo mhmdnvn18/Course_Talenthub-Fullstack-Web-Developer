135c98b5528ed021d48d18adad0458d0601ef3b9
anop <81241987+mhmdnvn18@users.noreply.github.com>
2024-12-04 17:41:36 +0700

    update

---
+++ Day2/script.js
@@ -0,0 +1,17 @@
+<script>
+//console.log("100");
+
+
+var number
+let kataKataHariIni
+
+//assigment=> mengisi variabel 
+number=100
+kataKataHariIni = 'Paham?'
+
+
+
+//reasigment
+number=100
+
+</script>
