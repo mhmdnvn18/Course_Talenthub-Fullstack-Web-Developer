b2526e87c1c5f1d4c9094c2d15198fc86419dd43
anop <81241987+mhmdnvn18@users.noreply.github.com>
2024-12-04 17:44:27 +0700

    update

---
+++ Day2/index.js
@@ -0,0 +1,16 @@
+// console.log("100");
+
+let number;
+let kataKataHariIni;
+
+// assignment => mengisi variabel 
+number = 100;
+kataKataHariIni = 'Paham?';
+
+// re-assignment => mengganti nilai variabel
+number = 200; // Mengganti nilai variabel dari 100 ke 200
+
+
+//deklarasi + assigment
+let student = 'maria'
+
